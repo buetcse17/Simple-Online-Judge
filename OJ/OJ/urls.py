a6b644ebbb5fa8471d9579b3a4abe3a78408a460
mahdihasnat <44502454+mahdihasnat@users.noreply.github.com>
2020-10-28 19:53:05 +0600

    modified signup template , view , added user app

--- OJ/OJ/urls.py
+++ OJ/OJ/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('' , include('index.urls')),
     path('' , include('signup.urls')),
+    path('user/' , include('user.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- OJ/signup/views.py
+++ OJ/signup/views.py
@@ -1,32 +1,18 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-
-
-from django.shortcuts import render
-from django.contrib.auth import login, authenticate
-from django.shortcuts import render, redirect
-
-class SignUpForm(UserCreationForm):
-    username = forms.CharField(max_length=30 )
-    email = forms.EmailField(max_length=200)
-
-    class Meta:
-        model = User
-        fields = ('username','first_name' ,  'email', 'password1', 'password2', )
+from django.contrib import messages
+from django.shortcuts import redirect
 
 def signup(request):
-    form = SignUpForm(request.POST)
-    if form.is_valid():
-        form.save()
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password1')
-        user = authenticate(username=username, password=password)
-        login(request, user)
-        return redirect('index')
-    else:
-        form = SignUpForm()
-    return render(request , 'signup.html' , {'form' : form })
+    if request.method == 'POST':
+        handle = request.POST['handle']
+        email = request.POST['email']
+        name = request.POST['name']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        
+        print (handle , email , name , password1 , password2 )
+
+        return redirect("/")
+
+    else :
+        return render(request , 'signup.html' )
