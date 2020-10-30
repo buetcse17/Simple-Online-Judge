733c987751f0a241a253a559cb1ead07b5d7369c
mahdihasnat <44502454+mahdihasnat@users.noreply.github.com>
2020-10-30 06:43:22 +0600

    Added country,inst...ion wise searching in ratings

--- OJ/ratings/models.py
+++ OJ/ratings/models.py
@@ -3,12 +3,48 @@ from django.db import models , connection
 # Create your models here.
 
 def get_ratings():
-    cursor = connection.cursor()
-    sql = "SELECT oj.USERs.HANDLE , COUNT(oj.participant.contest_id) , oj.USERS.RATING \
- FROM oj.users left join oj.participant ON (oj.USERS.USER_ID = oj.participant.USER_ID ) \
- GROUP BY oj.users.USER_ID , oj.USERs.HANDLE , oj.USERS.RATING \
- ORDER BY oj.USERS.RATING desc  , count(*) desc , oj.users.handle asc;"
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    cursor.close()
-    return result
+   """
+      list of tuple of ( handle , total_contest_participation , rating )
+   """
+   cursor = connection.cursor()
+   sql = "SELECT oj.USERs.HANDLE , COUNT(oj.participant.contest_id) , oj.USERS.RATING \
+FROM oj.users left join oj.participant ON (oj.USERS.USER_ID = oj.participant.USER_ID ) \
+GROUP BY oj.users.USER_ID , oj.USERs.HANDLE , oj.USERS.RATING \
+ORDER BY oj.USERS.RATING desc  , count(*) desc , oj.users.handle asc;"
+   cursor.execute(sql)
+   result = cursor.fetchall()
+   cursor.close()
+   return result
+
+
+def get_country_ratings(country_id):
+   """
+      list of tuple of ( handle , total_contest_participation , rating )
+   """
+   cursor = connection.cursor()
+   sql = f"SELECT oj.USERs.HANDLE , COUNT(oj.participant.contest_id) , oj.USERS.RATING \
+FROM oj.users left join oj.participant ON (oj.USERS.USER_ID = oj.participant.USER_ID ) \
+WHERE oj.USERS.country_id = {country_id}  \
+GROUP BY oj.users.USER_ID , oj.USERs.HANDLE , oj.USERS.RATING \
+ORDER BY oj.USERS.RATING desc  , count(*) desc , oj.users.handle asc;"
+   cursor.execute(sql)
+   result = cursor.fetchall()
+   cursor.close()
+   return result
+
+
+def get_institution_ratings(institution_id):
+   """
+      list of tuple of ( handle , total_contest_participation , rating )
+   """
+   cursor = connection.cursor()
+   sql = f"SELECT oj.USERs.HANDLE , COUNT(oj.participant.contest_id) , oj.USERS.RATING \
+FROM oj.users left join oj.participant ON (oj.USERS.USER_ID = oj.participant.USER_ID ) \
+WHERE oj.USERS.institution_id = {institution_id}  \
+GROUP BY oj.users.USER_ID , oj.USERs.HANDLE , oj.USERS.RATING \
+ORDER BY oj.USERS.RATING desc  , count(*) desc , oj.users.handle asc;"
+   cursor.execute(sql)
+   result = cursor.fetchall()
+   cursor.close()
+   return result
+
--- OJ/ratings/views.py
+++ OJ/ratings/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from user.models import get_country , get_institution
-from .models import get_ratings
+from .models import get_ratings , get_country_ratings , get_institution_ratings
 # Create your views here.
 
 def ratings(request):
@@ -12,3 +12,30 @@ def ratings(request):
     context['institutions'] = get_institution()
 
     return render(request , 'ratings.html' , context=context)
+def country_ratings(request , country_id):
+    context = {}
+    context['users'] = get_country_ratings(country_id=country_id)
+    context['countries'] =  get_country()
+    context['institutions'] = get_institution()
+    context['selected_country'] = country_id 
+    try:
+        del context['selected_institution']
+    except:
+        pass
+
+    return render(request , 'ratings.html' , context=context)
+
+def institution_ratings(request , institution_id):
+    context = {}
+    context['users'] = get_institution_ratings(institution_id=institution_id)
+    context['countries'] =  get_country()
+    context['institutions'] = get_institution()
+    context['selected_institution'] = institution_id 
+    try:
+        del context['selected_country']
+    except:
+        pass
+
+    return render(request , 'ratings.html' , context=context)
+
+
