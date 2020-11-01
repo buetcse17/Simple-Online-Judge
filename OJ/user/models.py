4c3f720901d8999e1f99400deb6ecd9d287d90d2
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2020-11-01 18:44:24 +0600

    Formating in User/Models.py

--- OJ/user/models.py
+++ OJ/user/models.py
@@ -6,91 +6,101 @@ import hashlib
 from OJ.utils import log_sql
 # Create your models here.
 
+
 def login(request, handle):
     """
     docstring
     """
     request.session['handle'] = handle
-    return 
+    return
+
+
 def logout(request):
     """
     docstring
     """
     try:
         del request.session['handle']
-    except :
+    except:
         pass
-    return 
-def authenticate(handle , password_hash):
+    return
+
+
+def authenticate(handle, password_hash):
     """ 
         authenticate with handle and password_hash
         returns True if successful
         else returns False
     """
-    cursor  = connection.cursor()
+    cursor = connection.cursor()
     sql = "select count(*) from oj.users where handle = %s and password_hash = %s"
-    cursor.execute(sql , [handle  , password_hash])
-    
-    #print(sql.format(handle  , password_hash)) 
+    cursor.execute(sql, [handle, password_hash])
+
+    #print(sql.format(handle  , password_hash))
     print(connection.queries)
-    
+
     result = cursor.fetchone()[0]
-    print(handle  , password_hash , result)
+    print(handle, password_hash, result)
     return result == 1
+
 
 def handle_exists(handle):
     """
         check if user exists with specific handle
 
     """
-    cursor  = connection.cursor()
+    cursor = connection.cursor()
     sql = "select count(*) from oj.users where handle = %s "
-    cursor.execute(sql , [handle ])
+    cursor.execute(sql, [handle])
     result = cursor.fetchone()[0]
     #print(handle , result)
     return result == 1
+
 
 def email_exists(email):
     """
         check if user exists with specific handle
 
     """
-    cursor  = connection.cursor()
+    cursor = connection.cursor()
     sql = "select count(*) from oj.users where email = %s "
-    cursor.execute(sql , [email ])
+    cursor.execute(sql, [email])
     result = cursor.fetchone()[0]
-    print(email , result)
+    print(email, result)
     return result == 1
 
-def add_user(handle ,  name, email , password_hash):
+
+def add_user(handle,  name, email, password_hash):
     """
         insert user into oj.users
     """
-    print(handle ,  name, email , password_hash)
+    print(handle,  name, email, password_hash)
     cursor = connection.cursor()
-    result  = False 
-    try :
+    result = False
+    try:
         sql = f"insert into oj.USERS(USER_ID, HANDLE, USER_NAME, EMAIL, PASSWORD_HASH ) \
             values ( user_id_seq.nextval , '{handle}' , '{name}' , '{email}' , '{password_hash}');"
-        cursor.execute(sql )
+        cursor.execute(sql)
         print(sql)
         log_sql(sql)
         connection.commit()
-        result = True 
+        result = True
 
-    except Exception as e :
-        print (e) 
+    except Exception as e:
+        print(e)
     finally:
         cursor.close()
     return result
 
 
-def get_hash(password ):
+def get_hash(password):
     """
         hashlib.sha256(password).hexdigest()
     """
     return hashlib.sha256(password.encode('utf-8')).hexdigest()
-def get_follower_count( user_id ):
+
+
+def get_follower_count(user_id):
     """
         total follower 
     """
@@ -101,7 +111,8 @@ def get_follower_count( user_id ):
     cursor.close()
     return result
 
-def get_user_id(handle ):
+
+def get_user_id(handle):
     """
         get id from handle
     """
@@ -112,7 +123,8 @@ def get_user_id(handle ):
     cursor.close()
     return result
 
-def get_user_information( handle ):
+
+def get_user_information(handle):
     """
           user_id ,user_name ,   rating , rating_catagory ,color , country_id , country_name ,institution_id , institution_name , profile_picture_location
     """
@@ -126,9 +138,10 @@ def get_user_information( handle ):
     cursor.execute(sql)
     result = cursor.fetchone()
 
-    print (result)
-    
+    print(result)
+
     return result
+
 
 def get_country():
     """
@@ -140,6 +153,8 @@ def get_country():
     result = cursor.fetchall()
     cursor.close()
     return result
+
+
 def get_institution():
     """
         sql result of institution_id , institution_name
