aebde722ead64f5c65a37be3499dd44913572032
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2020-11-07 00:52:13 +0600

    Added message view functionality ,issue timezone

--- OJ/message/models.py
+++ OJ/message/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 import OJ.utils
 # Create your models here.
 
+
 def get_total_new_messages(user_id):
     """
         return total_new_messages from user_id
@@ -17,12 +18,13 @@ def get_total_new_messages(user_id):
 
     return result
 
-def add_message(sender_id , receiver_id ,text ,  attachment_location ):
+
+def add_message(sender_id, receiver_id, text,  attachment_location):
     """
         add message to database 
     """
     time = timezone.now()
-    print('year ' ,time.year)
+    """ print('year ' ,time.year)
     print('year ' ,time.month)
     print('year ' ,time.day)
     print('year ' ,time.hour)
@@ -33,24 +35,81 @@ def add_message(sender_id , receiver_id ,text ,  attachment_location ):
     print("sender_id: " , sender_id)
     print("receiver_id: " , receiver_id)
     print("text: " , text)
-    print("attachment_location: " , attachment_location)
-    
+    print("attachment_location: " , attachment_location) """
+
     if len(text) == 0:
         text = ' '
     cursor = connection.cursor()
-    if attachment_location is None :
+    if attachment_location is None:
         sql = f"insert into oj.message(message_id , sender_id , receiver_id , text , time ) \
             values( oj.message_id_seq.nextval , {sender_id} , {receiver_id} , '{text}' , \
                  to_date( '{time.year}-{time.month}-{time.day}-{time.hour}-{time.minute}-{time.second}'   , 'YYYY-MM-DD-HH24-MI-SS' )  ) ;"
-    else :
+    else:
         sql = f"insert into oj.message(message_id , sender_id , receiver_id , text , time , attachment_location) \
             values( oj.message_id_seq.nextval , {sender_id} , {receiver_id} , '{text}' , \
                 to_date( '{time.year}-{time.month}-{time.day}-{time.hour}-{time.minute}-{time.second}'   , 'YYYY-MM-DD-HH24-MI-SS' ) ,  \
                     '{attachment_location}' ) ;"
-    print(sql)
+
     cursor.execute(sql)
     OJ.utils.log_sql(sql)
     cursor.close()
 
+    return
 
-    return 
+
+def set_seen_true(sender_id, receiver_id):
+    """
+    set seen 1 to all message between sender and receiver
+    """
+
+    sql = f"update oj.message set seen = 1 where sender_id = {sender_id} and receiver_id = {receiver_id} ;"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    OJ.utils.log_sql(sql)
+    cursor.close()
+
+    return
+
+
+def get_messages_all(user_id):
+    """
+        sender_handle ,sender_color , receiver_handle , receiver_color , text , attachment  , time[sent] , seen 
+    """
+
+    
+    sql = f"select (select handle from oj.users where user_id  = sender_id ) as sender_handle ,\
+            (select color from oj.Rating_Distribution where minimum_rating <= (select rating from oj.users where user_id = sender_id )  \
+            and maximum_rating >= (select rating from oj.users where user_id = sender_id ) )  as sender_color ,  \
+                (select handle from oj.users where user_id  = receiver_id ) as receiver_handle , \
+            (select color from oj.Rating_Distribution where minimum_rating <= (select rating from oj.users where user_id = receiver_id )  \
+            and maximum_rating >= (select rating from oj.users where user_id = receiver_id ) )  as receiver_color  ,\
+                text  , attachment_location , time , seen \
+     from oj.message where sender_id ={user_id} or receiver_id = { user_id }  order by time desc;"
+
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.close()
+
+    return result
+
+
+def get_messages_with(sender_id, receiver_id):
+    """
+        sender_handle ,sender_color , receiver_handle , receiver_color , text , attachment  , time[sent] , seen 
+    """
+    cursor = connection.cursor()
+    sql = f"select (select handle from oj.users where user_id  = sender_id ) as sender_handle ,\
+            (select color from oj.Rating_Distribution where minimum_rating <= (select rating from oj.users where user_id = sender_id )  \
+            and maximum_rating >= (select rating from oj.users where user_id = sender_id ) )  as sender_color ,  \
+                (select handle from oj.users where user_id  = receiver_id ) as receiver_handle , \
+            (select color from oj.Rating_Distribution where minimum_rating <= (select rating from oj.users where user_id = receiver_id )  \
+            and maximum_rating >= (select rating from oj.users where user_id = receiver_id ) )  as receiver_color  ,\
+                text  , attachment_location , time , seen \
+     from oj.message where sender_id in ( {sender_id} , {receiver_id} ) and receiver_id in ({ receiver_id } , {sender_id} ) order by time asc;"
+
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.close()
+
+    return result
--- OJ/message/views.py
+++ OJ/message/views.py
@@ -5,14 +5,15 @@ from django.core.files.storage import FileSystemStorage
 from user.models import is_loggedin
 from OJ.utils import add_user_information
 from user.models import handle_exists , get_user_id
-from .models import add_message
+from .models import add_message , get_messages_all  ,set_seen_true , get_messages_with
 # Create your views here.
 
 
 def messages(request):
     if is_loggedin(request=request):
         context = {}
-
+        context['messages'] = get_messages_all(request.session['user_id'])
+        
         context = add_user_information(request=request, context=context)
         return render(request, 'messages.html', context=context)
     else:
@@ -36,6 +37,10 @@ def conversation(request, handle):
                 text = request.POST['text']
 
                 add_message(sender_id=sender_id , receiver_id=receiver_id , text= text , attachment_location=attachment_location)
+
+            set_seen_true(sender_id= get_user_id(handle=handle) , receiver_id= request.session['user_id'])
+
+            context['messages'] = get_messages_with(sender_id= request.session['user_id']  , receiver_id= get_user_id(handle))
             context = add_user_information(request=request, context=context)
             return render(request, 'conversation.html', context=context)
         else:
