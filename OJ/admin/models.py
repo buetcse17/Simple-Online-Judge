2a140303b9fda8e22cddc0c27e90a61595fb7c97
Solaiman Ahmed <1705022@ugrad.cse.buet.ac.bd>
2020-11-27 23:23:31 +0600

    admin view changed

--- OJ/admin/models.py
+++ OJ/admin/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from user.models import is_loggedin , get_user_id
 
 # Create your models here.
+
+def is_admin(request, handle):
+    """
+        authenticate admin, if username == 'admin'
+    """
+    if(is_loggedin(request=request)==True):
+        if(handle=='admin'):
+            return True
+        else:
+            return False
+
+
+
--- OJ/admin/views.py
+++ OJ/admin/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+
+from user.models import is_loggedin
 
 # Create your views here.
+
+def admin(request):
+    if is_loggedin(request):
+        return render(request, 'admin/layout.html')
+    else:
+        return 
